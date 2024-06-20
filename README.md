82a2a81128554bfc508c466d6ecc4218842d74dd
Tiane Zerba <148304153+tianenaomi@users.noreply.github.com>
2024-06-20 16:53:55 +1000

    Add logic to clear last operation automatically

--- README.md
+++ README.md
@@ -2,29 +2,29 @@
 TOP Calculator Project
 Here are some use cases (abilities your project needs to have):
 
-Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, so start by creating functions for the following items and testing them in your browser’s console.
+Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, so start by creating functions for the following items and testing them in your browser’s console. - DONE
 
 - add
 - subtract
 - multiply
 - divide
 
-A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
+A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later. - DONE
 
-Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers. -DONE
 
-Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key.
+Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key. - DONE
 
 Do not worry about wiring up the JS just yet.
 
 There should also be a display for the calculator. Go ahead and fill it with some dummy numbers so it looks correct.
-Add a “clear” button.
+Add a “clear” button. - DONE
 
-Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step. - DONE
 
-Make the calculator work! You’ll need to store the first number and second number that are input into the calculator, utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key.
+Make the calculator work! You’ll need to store the first number and second number that are input into the calculator, utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key. DONE
 
-You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
+You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation. DONE
 
 This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
 
--- resources/script.js
+++ resources/script.js
@@ -1,77 +1,90 @@
-const one = document.getElementById('1').addEventListener('click', () => pressNum(1));
-const two = document.getElementById('2').addEventListener('click', () => pressNum(2));
-const three = document.getElementById('3').addEventListener('click', () => pressNum(3));
-const four = document.getElementById('4').addEventListener('click', () => pressNum(4));
-const five = document.getElementById('5').addEventListener('click', () => pressNum(5));
-const six = document.getElementById('6').addEventListener('click', () => pressNum(6));
-const seven = document.getElementById('7').addEventListener('click', () => pressNum(7));
-const eight = document.getElementById('8').addEventListener('click', () => pressNum(8));
-const nine = document.getElementById('9').addEventListener('click', () => pressNum(9));
-const zero = document.getElementById('0').addEventListener('click', () => pressNum(0));
-const integer = document.querySelectorAll('.integer');
-const operator = document.querySelectorAll('.operator');
+const one = document.getElementById('1').addEventListener('click', () => displayNum(1));
+const two = document.getElementById('2').addEventListener('click', () => displayNum(2));
+const three = document.getElementById('3').addEventListener('click', () => displayNum(3));
+const four = document.getElementById('4').addEventListener('click', () => displayNum(4));
+const five = document.getElementById('5').addEventListener('click', () => displayNum(5));
+const six = document.getElementById('6').addEventListener('click', () => displayNum(6));
+const seven = document.getElementById('7').addEventListener('click', () => displayNum(7));
+const eight = document.getElementById('8').addEventListener('click', () => displayNum(8));
+const nine = document.getElementById('9').addEventListener('click', () => displayNum(9));
+const zero = document.getElementById('0').addEventListener('click', () => displayNum(0));
 
-const clear = document.getElementById('clear').addEventListener('click', () => {
-    display.textContent = "0"
-});
-const decimal = document.getElementById('.').addEventListener('click', () => {
-    display.textContent += "."
-});
+const clear = document.getElementById('clear');
 const display = document.getElementById('display');
-const divide = document.getElementById('/').addEventListener('click', () => {
-    display.textContent += "/"
-    // add logic to display single operator only
-});
-const equals = document.getElementById('=').addEventListener('click', () => {
-    display.textContent = "=" //PLACEHOLDER: actually don't want this displayed. 
-});
-const minus = document.getElementById('-').addEventListener('click', () => {
-    display.textContent += "-"
-    // add logic to display single operator only
-});
-const mutliply = document.getElementById('*').addEventListener('click', () => {
-    display.textContent += "*"
-    // add logic to display single operator only
-});
-const plus = document.getElementById('+').addEventListener('click', () => {
-    display.textContent += "+"
-    // add logic to display single operator only
-});
+const divide = document.getElementById('/').addEventListener('click', () => displayOperator('/'));
+const equals = document.getElementById('=');
+const minus = document.getElementById('-').addEventListener('click', () => displayOperator('-'));
+const mutliply = document.getElementById('*').addEventListener('click', () => displayOperator('*'));
+const plus = document.getElementById('+').addEventListener('click', () => displayOperator('+'));
 
-let aNum = "";
-let bNum = "";
-let result;
 
-function pressNum(num){
-    display.textContent == 0 ? display.textContent = num : display.textContent += num;
-    aNum == "" ? aNum = num : bNum = num; 
+let aNum = '';
+let bNum = '';
+let operator = '';
+let result = '';
+let displayValue;
+
+function add(aNum, bNum){
+    return result = aNum + bNum;
 }
 
-plus.addEventListener('click', (aNum, bNum) => {
-    result = aNum + bNum;
-});
-
-equals.addEventListener('click', (result) => {
-    result;
-    // result to be added to display 
-});
-
-function operate(){
-
+function divideBy(aNum, bNum){
+    return result = aNum / bNum;
 }
 
-/*
-POUR MOI POUR LE LUNDI
-- started on logic for adding integers to either aNum or bNum
-- began equals event listener
+function subtract(aNum, bNum){
+    return result = aNum - bNum;
+}
 
-*/
+function multiply(aNum, bNum){
+    return result = aNum * bNum;
+}
 
-/*
-PSEUDOCODE
-OBTAIN first num
-OBTAIN operator
-OBTAIN second num
-INVOKE equals
+function operate(aNum, bNum, operator){
+    if (operator == '+') {
+        add(aNum, bNum);
+    } else if (operator == '-'){
+        subtract(aNum, bNum);
+    } else if (operator == '*'){
+        multiply(aNum, bNum);
+    } else if (operator == '/'){
+        divideBy(aNum, bNum);
+    }
+    return display.textContent = result;
+    //add logic to clear last answer
+}
 
-*/
+function displayNum(num){
+    if (result != '') clearDisplay();
+    (display.textContent == 0 
+    || display.textContent == 'enter num' 
+    || display.textContent.includes('+') 
+    || display.textContent.includes('-') 
+    || display.textContent.includes('*') 
+    || display.textContent.includes('/'))
+        ? display.textContent = num : display.textContent += num;
+    aNum == '' ? aNum = num : bNum = num;
+}
+
+function displayOperator(operatorSelection){
+    if (display.textContent == 0 || display.textContent == '') {
+        display.textContent = 'enter num';
+    } else {
+        display.textContent = operatorSelection;
+        operator = operatorSelection;  
+    }
+    //add logic for CSS button pressed
+}
+
+equals.addEventListener('click', () => operate(aNum, bNum, operator));
+
+clear.addEventListener('click', () => clearDisplay());
+
+function clearDisplay() {
+    display.textContent = "0"
+    aNum = '';
+    bNum = '';
+    operator = ''; 
+    result = ''; 
+}
+
